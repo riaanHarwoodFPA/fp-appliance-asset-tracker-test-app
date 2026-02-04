c4394ca47679b2664701e2a2681b19a37881187d
Riaan Harwood <riaan.harwood@fisherpaykel.com>
2026-02-04 15:07:58 +1300

    Fixed errors and bugs in mock data code

--- src/data/mockAppliances.ts
+++ src/data/mockAppliances.ts
@@ -6,25 +6,25 @@ import type { Appliance } from "../models/Appliance";
 
 export const mockAppliances: Appliance[] = [
     {
-        id: "APL-001"; 
-        model: "REF710A";
-        serialNumber: "SN123456789"; 
-        warrantyExpiryDate: "2026-30-06";
-        status: "Active";
+        id: "APL-001",
+        model: "REF710A",
+        serialNumber: "SN123456789", 
+        warrantyExpiryDate: "2026-30-06",
+        status: "Active",
     },
 
     {
-        id: "APL-002"; 
-        model: "DISH428";
-        serialNumber: "SN2219833641"; 
-        warrantyExpiryDate: "2026-19-09";
-        status: "Active";
+        id: "APL-002",
+        model: "DISH428",
+        serialNumber: "SN2219833641",
+        warrantyExpiryDate: "2026-19-09",
+        status: "Active",
     },
     {
-        id: "APL-001"; 
-        model: "WASH123";
-        serialNumber: "SN11213488952"; 
-        warrantyExpiryDate: "2026-30-11";
-        status: "Active";
+        id: "APL-001",
+        model: "WASH123",
+        serialNumber: "SN11213488952",
+        warrantyExpiryDate: "2026-30-11",
+        status: "Active",
     }
 ]
